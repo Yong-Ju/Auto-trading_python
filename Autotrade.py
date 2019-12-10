3f2577d065f55afcc104f204cc0a79eefe3bf721
Yong-Ju <didix@naver.com>
2019-12-10 18:28:00 +0900

    update .gitignore

--- Autotrade.py
+++ Autotrade.py
@@ -12,11 +12,11 @@ dlg = timings.wait_until_passes(20, 0.5, lambda: app.window(title=title))
 
 pass_ctrl = dlg.Edit2
 pass_ctrl.set_focus()
-pass_ctrl.type_keys('')
+pass_ctrl.type_keys('pwd')
 
-# cert_ctrl = dlg.Edit3
-# cert_ctrl.set_focus()
-# cert_ctrl.type_keys('yyyy')
+cert_ctrl = dlg.Edit3
+cert_ctrl.set_focus()
+cert_ctrl.type_keys('lspwd')
 
 btn_ctrl = dlg.Button0
 btn_ctrl.click()
