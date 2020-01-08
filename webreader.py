34bffcd831e6e38ca2a6e199074dfe76ebd48802
Yong-Ju <didix@naver.com>
2020-01-08 20:52:33 +0900

    add web-scarping

--- webreader.py
+++ webreader.py
@@ -1,50 +1,79 @@
 from bs4 import BeautifulSoup
+import requests
 
+def get_3year_treasury():
+    url = "http://www.index.go.kr/strata/jsp/showStblGams3.jsp?stts_cd=288401&amp;idx_cd=2884&amp;freq=Y&amp;period=1998:2016"
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html5lib')
+    td_data = soup.select("tr td")
 
+    treasury_3year = {}
+    start_year = 1998
 
+    for x in td_data:
+        treasury_3year[start_year] = x.text
+        start_year += 1
 
-    class PyMon:
+    print(treasury_3year)
+    return treasury_3year
 
-    def get_3year_treasury():
-        url = "http://www.index.go.kr/strata/jsp/showStblGams3.jsp?stts_cd=288401&amp;idx_cd=2884&amp;freq=Y&amp;period=1998:2016"
-        html = requests.get(url).text
-        soup = BeautifulSoup(html, 'html5lib')
-        td_data = soup.select("tr td")
+def get_current_3year_treasury():
+    url = "http://info.finance.naver.com/marketindex/interestDailyQuote.nhn?marketindexCd=IRR_GOVT03Y&page=1"
+    html = requests.get(url).text
 
-        treasury_3year = {}
-        start_year = 1998
+    soup = BeautifulSoup(html, 'html5lib')
+    td_data = soup.select("tr td")
+    return td_data[1].text
 
-        for x in td_data:
-            treasury_3year[start_year] = x.text
-            start_year += 1
+def get_financial_statements(code):
+    re_enc = re.compile("encparam: '(.*)'", re.IGNORECASE)
+    re_id = re.compile("id: '([a-zA-Z0-9]*)' ?", re.IGNORECASE)
 
-        print(treasury_3year)
-        return treasury_3year
+    url = "https://companyinfo.stock.naver.com/v1/company/c1010001.aspx?cmp_cd={}".format(code)
+    html = requests.get(url).text
+    encparam = re_enc.search(html).group(1)
+    encid = re_id.search(html).group(1)
 
-    def get_estimated_dividend_yield(code):
-        dividend_yield = get_financial_statements(code)
-        dividend_yield = sorted(dividend_yield.items())[-1]
-        return dividend_yield[1]
+    url = "https://companyinfo.stock.naver.com/v1/company/ajax/cF1001.aspx?cmp_cd={}&fin_typ=0&freq_typ=A&encparam={}&id={}".format(code, encparam, encid)
+    headers = {"Referer": "HACK"}
+    html = requests.get(url, headers=headers).text
 
-    def get_dividend_yield(code):
-        url = "http://companyinfo.stock.naver.com/company/c1010001.aspx?cmp_cd=" + code
-        html = requests.get(url).text
+    soup = BeautifulSoup(html, "html5lib")
+    dividend = soup.select("table:nth-of-type(2) tr:nth-of-type(33) td span")
+    years = soup.select("table:nth-of-type(2) th")
 
-        soup = BeautifulSoup(html, 'html5lib')
-        dt_data = soup.select("td dl dt")
+    dividend_dict = {}
+    for i in range(len(dividend)):
+        dividend_dict[years[i+3].text.strip()[:4]] = dividend[i].text
 
-        dividend_yield = dt_data[-2].text
-        dividend_yield = dividend_yield.split(' ')[1]
-        dividend_yield = dividend_yield[:-1]
+    return dividend_dict
 
-        return dividend_yield
+def get_estimated_dividend_yield(code):
+    dividend_yield = get_financial_statements(code)
+    dividend_yield = sorted(dividend_yield.items())[-1]
+    return dividend_yield[1]
+
+def get_dividend_yield(code):
+    url = "http://companyinfo.stock.naver.com/company/c1010001.aspx?cmp_cd=" + code
+    html = requests.get(url).text
+
+    soup = BeautifulSoup(html, 'html5lib')
+    dt_data = soup.select("td dl dt")
+
+    dividend_yield = dt_data[-2].text
+    dividend_yield = dividend_yield.split(' ')[1]
+    dividend_yield = dividend_yield[:-1]
+
+    return dividend_yield
 
 
 if __name__ == "__main__":
     # get_3year_treasury()
 
-    # dividend_yield = get_dividend_yield('058470')
-    # print(dividend_yield)
+    dividend_yield = get_dividend_yield('058470')
+    print(f'dividend_yield : {dividend_yield}')
+    # print(f'cur_3year_treasury : {get_current_3year_treasury()}')
 
-    estimated_dividend_yield = get_estimated_dividend_yield('058470')
-    print(estimated_dividend_yield)
+    #
+    # estimated_dividend_yield = get_estimated_dividend_yield('058470')
+    # print(estimated_dividend_yield)
